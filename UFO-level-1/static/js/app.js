6f2d9641da7a8dcdd4826a7933df13ee554bd839
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-06-22 21:17:51 -0700

    referenced table body, appended `tr` elements

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,14 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Loop through tableData and console.log each alien sighting object
+tableData.forEach(function(alienSightings) {
+    console.log(alienSightings);
+
+    // Use d3 to append one table row 'tr' for each sighting
+    var row = tbody.append("tr");  // appends 'tr' elements.
+});
