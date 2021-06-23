769e6ea75583d773b2ac4f9a146f53813c2b1a00
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-06-22 22:07:28 -0700

    added code to filter data with Filter Table button

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -24,3 +24,34 @@ tableData.forEach((alienSightings) => {
         cell.text(value);
     });
 });
+
+// Get reference to the Filter Table button
+var button = d3.select(".btn-default");
+
+// Use d3 '.on' to attach a click handler for the filter table
+button.on("click", function() {
+    // Prevent page from refreshing
+    d3.event.preventDefault();
+    // Remove any data
+    tbody.html("");
+
+    // Select the input element and get the raw html node
+    var inputElement = d3.select(".form-control");
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+
+    // Clear table from previous query
+    tbody.selectAll("tr").remove();
+    // console.log(inputValue)
+    var filteredData = tableData.filter(sighting => sighting.datetime === inputValue);
+
+    console.log(filteredData)
+    // Use loop to append filtered data to table
+    filteredData.forEach((date) => {
+        var row = tbody.append("tr");
+        Object.entries(date).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+});
