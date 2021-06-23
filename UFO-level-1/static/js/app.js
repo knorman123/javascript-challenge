064341ab61803e00901fb559497db0673f07bae8
knorman123 <55822444+knorman123@users.noreply.github.com>
2021-06-22 21:24:25 -0700

    populated table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -12,4 +12,15 @@ tableData.forEach(function(alienSightings) {
 
     // Use d3 to append one table row 'tr' for each sighting
     var row = tbody.append("tr");  // appends 'tr' elements.
+
+    // Use 'Object.entries' to console.log each alien sighting value
+    Object.entries(alienSightings).forEach(function([key, value]) {
+        console.log(key, value);
+
+        // Append a cell to the row for each value in the sighting object
+        var cell = row.append("td");
+
+        // Add text
+        cell.text(value);
+    });
 });
